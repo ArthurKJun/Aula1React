4599f336ad66995c7b39023e6399872175464fc6
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2023-11-29 20:45:24 -0300

    Criado a tela de consultaContato com table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CadContato from './componentes/cadContato'
+import ConsultaContato from './componentes/consultaContato';
 
 function App() {
   return (
     <div className="container">
         
-      <CadContato />
+      <ConsultaContato />
 
     </div>
   );
---
+++ src/componentes/consultaContato.js
@@ -0,0 +1,35 @@
+export default function ConsultaContato(){
+    return(
+        <>
+        <h1>Tabela de Usuários</h1>
+        
+        <table className="table table-dark table-hover table-striped" >
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Maria</td>
+                    <td>maria@gmai.com</td>
+                    <td>47999999999</td>                
+                </tr>
+                <tr>
+                    <td>joão</td>
+                    <td>joao@gmai.com</td>
+                    <td>47999999998</td>                
+                </tr>
+                <tr>
+                    <td>Alfredo</td>
+                    <td>alfredinho@gmai.com</td>
+                    <td>47991222232</td>                
+                </tr>
+            </tbody>
+        </table>
+     
+        </>
+    )
+}
